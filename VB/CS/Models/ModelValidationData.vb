be3c6eacbfb8fa1d537e07e7de28ed5c83e4fee2
DevExpressExampleBot <fakeEmail>
2018-08-31 17:57:17 +0300

    Source auto update [skip ci]

---
+++ VB/CS/Models/ModelValidationData.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel.DataAnnotations
+Imports System.Linq
+Imports System.Web
+
+Namespace CS.Models
+    Public Class ModelValidationData
+        <Display(Name := "Name:"), Required(ErrorMessage := "Name is required"), StringLength(50, ErrorMessage := "Must be under 50 characters")> _
+        Public Property Name() As String
+
+        <Display(Name := "Age:"), Range(18, 100, ErrorMessage := "Must be between 18 and 100")> _
+        Public Property Age() As Integer?
+
+        <Display(Name := "Email:"), Required(ErrorMessage := "Email is required"), RegularExpression("\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage := "Email is invalid")> _
+        Public Property Email() As String
+
+        <Display(Name := "Arrival Date:"), Required(ErrorMessage := "Arrival date is required")> _
+        Public Property ArrivalDate() As Date?
+    End Class
+End Namespace
